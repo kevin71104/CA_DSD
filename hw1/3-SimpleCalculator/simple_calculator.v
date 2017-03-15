a7b58c559f960d3608cbf127e468f0507d50aaf9
kevin71104 <b03901026@ntu.edu.tw>
2017-03-16 02:11:05 +0800

    mod MUX

--- hw1/3-SimpleCalculator/simple_calculator.v
+++ hw1/3-SimpleCalculator/simple_calculator.v
@@ -1,18 +1,7 @@
 `include "alu.v"
 `include "register_file.v"
 
-module simple_calculator(
-    Clk,
-    WEN,
-    RW,
-    RX,
-    RY,
-    DataIn,
-    Sel,
-    Ctrl,
-    busY,
-    Carry
-);
+module simple_calculator(Clk, WEN, RW, RX, RY, DataIn, Sel, Ctrl, busY, Carry);
 
     input        Clk;
     input        WEN;
@@ -27,28 +16,14 @@ module simple_calculator(
     wire        carry_tmp;
     wire [7:0]  busW_tmp, busX_tmp, busY_tmp, x, y;
 // submodule instantiation
-    register_file rf(
-        Clk      ,
-        WEN      ,
-        RW       ,
-        busW_tmp ,
-        RX       ,
-        RY       ,
-        busX_tmp ,
-        busY_tmp
-    );
-    alu alu1(
-        Ctrl     ,
-        x        ,
-        y        ,
-        Carry    ,
-        busW_tmp ,
-    );
+    register_file rf(Clk, WEN, RW, busW_tmp, RX, RY, busX_tmp, busY_tmp);
+    alu alu1(Ctrl, x, y, Carry, busW_tmp);
+    
 // combinational part
     //assign x    = Sel ? busX_tmp : DataIn;
     always@(Sel,busX_tmp,DataIn) begin
         if (Sel) : x = busX_tmp;
-        else     : x = DataIn; 
+        else     : x = DataIn;
     end
     assign y    = busY_tmp;
     assign busY = busY_tmp;
