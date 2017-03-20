15b61099cd082883d1f6c6cef5c246714e7761a2
kevin71104 <b03901026@ntu.edu.tw>
2017-03-20 20:26:29 +0800

    finish bubble sort

--- hw2/Bubble_sort.s
+++ hw2/Bubble_sort.s
@@ -2,9 +2,10 @@
 
 .data
 str1:   .asciiz        "Bubble Sort\n"
-str2:   .asciiz        "\nData before sorting:\n"
+str2:   .asciiz        "Data before sorting:\n"
 str3:   .asciiz        "\nData after sorting:\n"
 str4:   .asciiz        ", "
+str5:   .asciiz        "\nEnd of Bubble Sort!!"
 .text
 
 
@@ -66,7 +67,10 @@ j          exit
 
 .globl printData
 printData:
-    beqz       $t1,   L1                   #if(i == 0) skip print ", "
+    addi       $sp,   $sp,    -4
+    sw         $s0    0($sp)               # put $s0 at $sp
+    move       $s0    $a1                  # move $a1 to $s0
+    beqz       $t1,   L1                   # if(i == 0) skip print ", "
     li         $v0,   4
     la         $a0,   str4                 # load ", "
     syscall
@@ -76,7 +80,9 @@ L1:
     syscall
     addi       $t0,   4                    # next element
     addi       $t1,   1                    # i = i + 1
-    blt        $t1,   $a1,    printData    # if(i<10) go to loop
+    blt        $t1,   $s0,    printData    # if(i<10) go to loop
+    lw         $s0,   0($sp)               # load data at $sp to $s0
+    addi       $sp,   $sp,    4
     jr         $ra                         # jump back to main
 
 
@@ -99,16 +105,26 @@ bubbleSort:
     sw         $s2,    8($sp)
     sw         $s1,    4($sp)
     sw         $s0,    0($sp)
-    move       $s2,    $a0             # store the address of array
-    move       $s3,    $a1             # store length
+    move       $s2,    $a0             # s2 = address of array
+    move       $s3,    $a1             # s3 = length
     li         $s0,    0               # i = 0
 fori:
-    bge        $s0,    $s3,    exiti
-    jr         $ra
-    addi       $s1,    $s0,    -1
-
+    bge        $s0,    $s3,    exiti   # if( i >= 10 ) jump to exiti
+    addi       $s1,    $s0,    -1      # j = i - 1
+forj:
+    blt        $s1,    $zero,  exitj   # if(j < 0) exit j-loop
+    sll        $t0,    $s1,    2       # $t0 = j * 4
+    add        $t1,    $t0,    $s2     # $t1 = v + 4*j
+    lw         $t2,    0($t1)          # $t2 = v[j]
+    lw         $t3,    4($t1)          # $t3 = v[j+1]
+    bge        $t3,    $t2,    exitj   # if(v[j+1] > v[j]) exit j-loop
+    move       $a0,    $s2             # parameter: address of array
+    move       $a1,    $s1             # parameter: index
+    jal        swap
+    addi       $s1,    $s1,    -1      # j = j - 1
+    j          forj
 exitj:
-    addi       $s0,    $s0,    1
+    addi       $s0,    $s0,    1       # i = i + 1
     j          fori
 
 exiti:
@@ -120,8 +136,25 @@ exiti:
     addi       $sp,    $sp,    20
 
     jr         $ra
+########## C++ code ###########
+#    temp = v[j];             #
+#    v[j] = v[j+1];           #
+#    v[j+1] = temp;           #
+###############################
+swap:
+    sll        $t0,    $a1,    2        # $t0 = j * 4
+    add        $t0,    $t0,    $a0      # $t0 = v + j * 4
+    lw         $t1,    0($t0)           # $t1 = v[j]
+    lw         $t2,    4($t0)           # $t2 = v[j+1]
+    sw         $t2,    0($t0)           # v[j] = $t2
+    sw         $t1,    4($t0)           # v[j+1] = $t1
+
+    jr         $ra
 
 .globl exit
 exit:
+    li         $v0,        4         # print string
+    la         $a0,        str5      # load string
+    syscall
     li         $v0,        10
     syscall
