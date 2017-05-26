b4a6c5b689c5299c83a7bb1e406d5fef543de919
kevin71104 <b03901026@ntu.edu.tw>
2017-05-26 20:17:04 +0800

    code finish, not yet simulated

--- hw5/cache.v
+++ hw5/cache.v
@@ -60,12 +60,11 @@ module cache(
     reg [127:0] mem_wdata, mem_wdata_next;
     reg  [31:0] proc_rdata, proc_rdata_next;
 
-    // MUX
+    // MUX-output
     reg  [24:0] tag;
     reg         valid;
     reg         dirty;
     reg  [31:0] blockdata[3:0];
-    reg  [31:0] block_nextdata[3:0];
 
     // divide proc_addr_r into 3 wire
     wire  [1:0] wordIndex;
@@ -97,10 +96,14 @@ module cache(
                     if(~mem_ready_r)begin
                         state_next = MISS;
                         proc_stall_next = 1'b1;
+                        mem_read_next   = 1'b1;
+                        mem_write_next  = 1'b0;
                     end
                     else begin
                         state_next = PROCESS;
                         proc_stall_next = 1'b0;
+                        mem_read_next   = 1'b0;
+                        mem_write_next  = 1'b0;
                     end
                 end
             HIT:
@@ -108,14 +111,20 @@ module cache(
                     if(hit)begin
                         state_next = PORCESS;
                         proc_stall_next = 1'b0;
+                        mem_read_next   = 1'b0;
+                        mem_write_next  = 1'b0;
                     end
                     else if(dirty && ~hit)begin
                         state_next = WB;
                         proc_stall_next = 1'b1;
+                        mem_read_next   = 1'b0;
+                        mem_write_next  = 1'b1;
                     end
-                    else begin
+                    else if(~dirty && ~hit)begin
                         state_next = MISS;
                         proc_stall_next = 1'b1;
+                        mem_read_next   = 1'b1;
+                        mem_write_next  = 1'b0;
                     end
                 end
             WB:
@@ -123,21 +132,29 @@ module cache(
                     if(mem_ready_r)begin
                         state_next = MISS;
                         proc_stall_next = 1'b1;
+                        mem_read_next   = 1'b1;
+                        mem_write_next  = 1'b0;
                     end
                     else begin
                         state_next = WB;
                         proc_stall_next = 1'b1;
+                        mem_read_next   = 1'b0;
+                        mem_write_next  = 1'b1;
                     end
                 end
             PROCESS:
                 begin
                     state_next = HIT;
                     proc_stall_next = 1'b1;
+                    mem_read_next   = 1'b0;
+                    mem_write_next  = 1'b0;
                 end
             default:
                 begin
                     state_next = PROCESS;
                     proc_stall_next = 1'b0;
+                    mem_read_next   = 1'b0;
+                    mem_write_next  = 1'b0;
                 end
         endcase
     end
@@ -216,61 +233,234 @@ module cache(
             3'd7:
                 {blockdata[3:0]} = {block[7][3:0]};
         endcase
-        case(blockIndex)
-            3'd0:
-                {block_next[0][3:0]} = {block_nextdata[3:0]};
-            3'd1:
-                {block_next[1][3:0]} = {block_nextdata[3:0]};
-            3'd2:
-                {block_next[2][3:0]} = {block_nextdata[3:0]};
-            3'd3:
-                {block_next[3][3:0]} = {block_nextdata[3:0]};
-            3'd4:
-                {block_next[4][3:0]} = {block_nextdata[3:0]};
-            3'd5:
-                {block_next[5][3:0]} = {block_nextdata[3:0]};
-            3'd6:
-                {block_next[6][3:0]} = {block_nextdata[3:0]};
-            3'd7:
-                {block_next[7][3:0]} = {block_nextdata[3:0]};
-        endcase
     end
 
-    //
+    // block data
     always@(*)begin
+        // get original FF content
         for(i=0;i<=7;i=i+1)begin
             {block_next[i][3:0]} = {block[i][3:0]};
             blockvalid_next[i] = blockvalid[i];
             blockdirty_next[i] = blockdirty[i];
             blocktag_next[i] = blocktag[i];
         end
-        if (state == PROCESS) begin
-            if(proc_read_r && ~proc_write_r)begin
-                case(wordIndex)
-                    2'd0:
-                        proc_rdata_next = blockdata[0];
-                    2'd1:
-                        proc_rdata_next = blockdata[1];
-                    2'd2:
-                        proc_rdata_next = blockdata[2];
-                    2'd3:
-                        proc_rdata_next = blockdata[3];
-                endcase
-            end
+        case(state)
+            PROCESS:
+                begin
+                    // READ
+                    if(proc_read_r && ~proc_write_r)begin
+                        case(wordIndex)
+                            2'd0:
+                                proc_rdata_next = blockdata[0];
+                            2'd1:
+                                proc_rdata_next = blockdata[1];
+                            2'd2:
+                                proc_rdata_next = blockdata[2];
+                            2'd3:
+                                proc_rdata_next = blockdata[3];
+                        endcase
+                    end
 
-            if(~proc_read_r && proc_write_r)begin
-                case(wordIndex)
-                    2'd0:
-                        block_nextdata[0] = proc_wdata_r;
-                    2'd1:
-                        block_nextdata[1] = proc_wdata_r;
-                    2'd2:
-                        block_nextdata[2] = proc_wdata_r;
-                    2'd3:
-                        block_nextdata[3] = proc_wdata_r;
-                endcase
-            end
-        end
+                    // WRITE
+                    if(~proc_read_r && proc_write_r)begin
+                        case(blockIndex)
+                            3'd0:
+                            case(wordIndex)
+                                2'd0:
+                                    block_next[0][0] = proc_wdata_r;
+                                2'd1:
+                                    block_next[0][1] = proc_wdata_r;
+                                2'd2:
+                                    block_next[0][2] = proc_wdata_r;
+                                2'd3:
+                                    block_next[0][3] = proc_wdata_r;
+                            endcase
+                            3'd1:
+                            case(wordIndex)
+                                2'd0:
+                                    block_next[1][0] = proc_wdata_r;
+                                2'd1:
+                                    block_next[1][1] = proc_wdata_r;
+                                2'd2:
+                                    block_next[1][2] = proc_wdata_r;
+                                2'd3:
+                                    block_next[1][3] = proc_wdata_r;
+                            endcase
+                            3'd2:
+                            case(wordIndex)
+                                2'd0:
+                                    block_next[2][0] = proc_wdata_r;
+                                2'd1:
+                                    block_next[2][1] = proc_wdata_r;
+                                2'd2:
+                                    block_next[2][2] = proc_wdata_r;
+                                2'd3:
+                                    block_next[2][3] = proc_wdata_r;
+                            endcase
+                            3'd3:
+                            case(wordIndex)
+                                2'd0:
+                                    block_next[3][0] = proc_wdata_r;
+                                2'd1:
+                                    block_next[3][1] = proc_wdata_r;
+                                2'd2:
+                                    block_next[3][2] = proc_wdata_r;
+                                2'd3:
+                                    block_next[3][3] = proc_wdata_r;
+                            endcase
+                            3'd4:
+                            case(wordIndex)
+                                2'd0:
+                                    block_next[4][0] = proc_wdata_r;
+                                2'd1:
+                                    block_next[4][1] = proc_wdata_r;
+                                2'd2:
+                                    block_next[4][2] = proc_wdata_r;
+                                2'd3:
+                                    block_next[4][3] = proc_wdata_r;
+                            endcase
+                            3'd5:
+                            case(wordIndex)
+                                2'd0:
+                                    block_next[5][0] = proc_wdata_r;
+                                2'd1:
+                                    block_next[5][1] = proc_wdata_r;
+                                2'd2:
+                                    block_next[5][2] = proc_wdata_r;
+                                2'd3:
+                                    block_next[5][3] = proc_wdata_r;
+                            endcase
+                            3'd6:
+                            case(wordIndex)
+                                2'd0:
+                                    block_next[6][0] = proc_wdata_r;
+                                2'd1:
+                                    block_next[6][1] = proc_wdata_r;
+                                2'd2:
+                                    block_next[6][2] = proc_wdata_r;
+                                2'd3:
+                                    block_next[6][3] = proc_wdata_r;
+                            endcase
+                            3'd7:
+                            case(wordIndex)
+                                2'd0:
+                                    block_next[7][0] = proc_wdata_r;
+                                2'd1:
+                                    block_next[7][1] = proc_wdata_r;
+                                2'd2:
+                                    block_next[7][2] = proc_wdata_r;
+                                2'd3:
+                                    block_next[7][3] = proc_wdata_r;
+                            endcase
+                        endcase
+                        case(blockIndex)
+                            3'd0:
+                                blockdirty_next[0] = 1'b1;
+                            3'd1:
+                                blockdirty_next[1] = 1'b1;
+                            3'd2:
+                                blockdirty_next[2] = 1'b1;
+                            3'd3:
+                                blockdirty_next[3] = 1'b1;
+                            3'd4:
+                                blockdirty_next[4] = 1'b1;
+                            3'd5:
+                                blockdirty_next[5] = 1'b1;
+                            3'd6:
+                                blockdirty_next[6] = 1'b1;
+                            3'd7:
+                                blockdirty_next[7] = 1'b1;
+                        endcase
+                    end
+                end
+            HIT:
+                begin
+                    if(~dirty && ~hit)begin
+                        mem_addr_next = {proc_addr_r[29:2]};
+                    end
+                    else if(dirty && ~hit)begin
+                        mem_addr_next = {tag[24:0],blockIndex[2:0]};
+                        mem_wdata_next = {blockdata[3:0]};
+                    end
+                end
+            MISS:
+                begin
+                    if(mem_ready_r)begin
+                        case(blockIndex)
+                            3'd0:
+                                {block_next[0][3:0]} = mem_rdata_r;
+                            3'd1:
+                                {block_next[1][3:0]} = mem_rdata_r;
+                            3'd2:
+                                {block_next[2][3:0]} = mem_rdata_r;
+                            3'd3:
+                                {block_next[3][3:0]} = mem_rdata_r;
+                            3'd4:
+                                {block_next[4][3:0]} = mem_rdata_r;
+                            3'd5:
+                                {block_next[5][3:0]} = mem_rdata_r;
+                            3'd6:
+                                {block_next[6][3:0]} = mem_rdata_r;
+                            3'd7:
+                                {block_next[7][3:0]} = mem_rdata_r;
+                        endcase
+                        case(blockIndex)
+                            3'd0:
+                                blocktag_next[0] = dataTag;
+                            3'd1:
+                                blocktag_next[1] = dataTag;
+                            3'd2:
+                                blocktag_next[2] = dataTag;
+                            3'd3:
+                                blocktag_next[3] = dataTag;
+                            3'd4:
+                                blocktag_next[4] = dataTag;
+                            3'd5:
+                                blocktag_next[5] = dataTag;
+                            3'd6:
+                                blocktag_next[6] = dataTag;
+                            3'd7:
+                                blocktag_next[7] = dataTag;
+                        endcase
+                        case(blockIndex)
+                            3'd0:
+                                blockdirty_next[0] = 1'b0;
+                            3'd1:
+                                blockdirty_next[1] = 1'b0;
+                            3'd2:
+                                blockdirty_next[2] = 1'b0;
+                            3'd3:
+                                blockdirty_next[3] = 1'b0;
+                            3'd4:
+                                blockdirty_next[4] = 1'b0;
+                            3'd5:
+                                blockdirty_next[5] = 1'b0;
+                            3'd6:
+                                blockdirty_next[6] = 1'b0;
+                            3'd7:
+                                blockdirty_next[7] = 1'b0;
+                        endcase
+                        case(blockIndex)
+                            3'd0:
+                                blockvalid_next[0] = 1'b1;
+                            3'd1:
+                                blockvalid_next[1] = 1'b1;
+                            3'd2:
+                                blockvalid_next[2] = 1'b1;
+                            3'd3:
+                                blockvalid_next[3] = 1'b1;
+                            3'd4:
+                                blockvalid_next[4] = 1'b1;
+                            3'd5:
+                                blockvalid_next[5] = 1'b1;
+                            3'd6:
+                                blockvalid_next[6] = 1'b1;
+                            3'd7:
+                                blockvalid_next[7] = 1'b1;
+                        endcase
+                    end
+                end
+        endcase
     end
 //==== sequential circuit =================================
     always@( posedge clk or posedge proc_reset ) begin
@@ -296,6 +486,7 @@ module cache(
             state <= state_next;
 
             //internal FF
+            {block[7:0][3:0]} <= {block_next[7:0][3:0]};
         end
     end
 
